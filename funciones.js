e39bb6c1614e3ae497aab58197183b7e31a213a7
Clara Hester <clarahester@Claras-MacBook-Air.local>
2020-11-14 09:32:39 -0600

    11-14-20

---
+++ funciones.js
@@ -0,0 +1,14 @@
+function comparar () {
+    
+    const a = 7
+    const b = 13
+
+    if (a>b) {
+        alert(a + ' es mayor que ' + b);
+    } else if (a<b) {
+        alert(a + ' es menor que ' +b);
+    } else {
+        alert(a + ' = ' + b);
+    }
+
+    }
